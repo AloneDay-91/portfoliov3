69bd1420707d6405f7cd2391b04d72be8dc4e371
bruz0003 <elouan.bruzek@etudiant.univ-reims.fr>
2025-02-11 22:31:27 +0100

    Modif icon + anim xp

--- src/components/XPPRO.tsx
+++ src/components/XPPRO.tsx
@@ -1,3 +1,5 @@
+import DecryptedText from "@/components/ui/TextAnimations/DecryptedText/DecryptedText.tsx";
+
 export function XPPRO(){
     return (
         <>
@@ -9,24 +11,24 @@ export function XPPRO(){
                 <div className="absolute inset-0 -z-10 h-full w-full dark:bg-[linear-gradient(to_right,#141414_1px,transparent_1px),linear-gradient(to_bottom,#141414_1px,transparent_1px)] bg-[linear-gradient(to_right,#f5f5f5_1px,transparent_1px),linear-gradient(to_bottom,#f5f5f5_1px,transparent_1px)] bg-[size:14px_24px]"></div>
                 <div className="flex items-start justify-start gap-8">
                     <div className="w-1/5">
-                        <span className="text-muted-foreground text-2xl">Juillet 2024</span>
+                        <span className="text-muted-foreground text-2xl"><DecryptedText animateOn="view" revealDirection="start" speed={80} sequential={true} text="Juillet 2024" /></span>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <h2 className="font-medium text-2xl">Jeux Olympique Paris 2024 - Châteauroux</h2>
+                        <h2 className="font-medium text-2xl"><DecryptedText animateOn="view" revealDirection="start" speed={50} sequential={true} text="Jeux Olympique Paris 2024 - Châteauroux" /></h2>
                         <div className="text-muted-foreground  leading-6 text-md">
-                            <p>Volontaire tir sportif Équipier armurerie</p>
+                            <p><DecryptedText animateOn="view" revealDirection="start" speed={50} sequential={true} text="Volontaire tir sportif Équipier armurerie" /></p>
                         </div>
                     </div>
                 </div>
                 <br/>
                 <div className="flex items-start justify-start gap-8">
                     <div className="w-1/4">
-                        <span className="text-muted-foreground text-2xl">Janvier 2018</span>
+                        <span className="text-muted-foreground text-2xl"><DecryptedText animateOn="view" revealDirection="start" speed={80} sequential={true} text="Janvier 2018" /></span>
                     </div>
                     <div className="flex flex-col gap-2 w-full">
-                        <h2 className="font-medium text-2xl">Air France KLM - Orly</h2>
+                        <h2 className="font-medium text-2xl"><DecryptedText animateOn="view" revealDirection="start" speed={50} sequential={true} text="Air France KLM - Orly" /></h2>
                         <div className="text-muted-foreground  leading-6 text-md">
-                            <p>Stage d’observation Département moteur</p>
+                            <p><DecryptedText animateOn="view" revealDirection="start" speed={50} sequential={true} text="Stage d’observation Département moteur" /></p>
                         </div>
                     </div>
                 </div>

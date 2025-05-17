5c6335cae3789117e795abd693667e28c920fd90
bruz0003 <elouan.bruzek@etudiant.univ-reims.fr>
2025-05-17 21:45:34 +0200

    Fix : footer non cliquable

--- src/App.tsx
+++ src/App.tsx
@@ -80,34 +80,32 @@ export default function App() {
                 <Projets/>
                 <DivideTitle title="Contact" id="contact"/>
                 <Contact/>
-                <footer className="max-w-screen-2xl mx-auto border-l border-r px-4 w-full h-20 flex items-center justify-between">
-                    <div>
-                        <p className="text-muted-foreground text-sm">© 2025 Elouan Bruzek. Tous droits réservés.</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Button variant="ghost" size="icon" asChild>
-                            <a href="https://www.linkedin.com/in/elouanbruzek/" target="_blank">
-                                <Linkedin strokeWidth={1.5} />
-                            </a>
-                        </Button>
-                        <Button variant="ghost" size="icon" asChild>
-                            <a href="https://github.com/AloneDay-91" target="_blank">
-                                <Github strokeWidth={1.5} />
-                            </a>
-                        </Button>
-                        <Button variant="ghost" size="icon" asChild>
-                            <ThemeToggle />
-                        </Button>
-                        <Button variant='outline' asChild>
-                            <a href="/CV_Elouan_Bruzek.pdf" target="_blank">
-                                Télécharger mon CV
-                            </a>
-                        </Button>
-                    </div>
-                </footer>
             </div>
-
-
+            <footer className="max-w-screen-2xl mx-auto border-l border-r px-4 w-full h-20 flex items-center justify-between">
+                <div>
+                    <p className="text-muted-foreground text-sm">© 2025 Elouan Bruzek. Tous droits réservés.</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Button variant="ghost" size="icon" asChild>
+                        <a href="https://www.linkedin.com/in/elouanbruzek/" target="_blank">
+                            <Linkedin strokeWidth={1.5} />
+                        </a>
+                    </Button>
+                    <Button variant="ghost" size="icon" asChild>
+                        <a href="https://github.com/AloneDay-91" target="_blank">
+                            <Github strokeWidth={1.5} />
+                        </a>
+                    </Button>
+                    <Button variant="ghost" size="icon" asChild>
+                        <ThemeToggle />
+                    </Button>
+                    <Button variant='outline' asChild>
+                        <a href="/CV_Elouan_Bruzek.pdf" target="_blank">
+                            Télécharger mon CV
+                        </a>
+                    </Button>
+                </div>
+            </footer>
         </>
     );
 }
